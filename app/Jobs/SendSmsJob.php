e07ba952c213f8271dc2c7f3a16356635565c8f8
ElchinMammadov <ekodrive@gmail.com>
2025-09-05 16:13:48 +0400

    sms sending 2 times fixed

--- app/Jobs/SendSmsJob.php
+++ app/Jobs/SendSmsJob.php
@@ -5,19 +5,39 @@ namespace App\Jobs;
 use App\Models\SmsLog;
 use App\Services\Client\SMS\SMSService;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Foundation\Queue\Queueable;
 
-class SendSmsJob implements ShouldQueue
+class SendSmsJob implements ShouldQueue, ShouldBeUnique
 {
     use Queueable;
 
     public array $data;
+
+    /**
+     * Maximum number of tries (only once).
+     */
+    public $tries = 1;
+
+    /**
+     * Timeout in seconds.
+     */
+    public $timeout = 10;
+
     /**
      * Create a new job instance.
      */
     public function __construct(array $data)
     {
         $this->data = $data;
+    }
+
+    /**
+     * Unique job identifier (avoid duplicate SMS).
+     */
+    public function uniqueId(): string
+    {
+        return $this->data['session_id'] . '_' . $this->data['phone'];
     }
 
     /**
@@ -29,11 +49,11 @@ class SendSmsJob implements ShouldQueue
 
         SmsLog::create([
             'merchant_id' => $this->data['merchant_id'],
-            'session_id' => $this->data['session_id'],
-            'phone' => $this->data['phone'],
-            'message' => $this->data['message'],
-            'status' => $result['status'],
-            'url' => $this->data['url'],
+            'session_id'  => $this->data['session_id'],
+            'phone'       => $this->data['phone'],
+            'message'     => $this->data['message'],
+            'status'      => $result['status'],
+            'url'         => $this->data['url'],
         ]);
     }
 }
